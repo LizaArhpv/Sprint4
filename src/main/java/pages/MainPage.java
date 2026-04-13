3c306101505926245370d362ccf82e9bdc25d633
Liza Arhipova <lizamakeeva001@yandex.ru>
2026-04-13 14:14:34 +0300

    Third commit

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,7 +2,6 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
@@ -20,18 +19,15 @@ public MainPage(WebDriver driver) {
     public static final By HEADER_ORDER_BUTTON = By.className("Button_Button__ra12g"); // Локатор кнопки "Заказать" вверху страницы
     public static final By FOOTER_ORDER_BUTTON = By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div[5]/button"); // Локатор кнопки "Заказать" посередине страницы/ другого способа обозначить не нашла
 
+    // Вопрос FAQ по индексу
+    public By faqQuestionByIndex(int index) {
+        return By.id("accordion__heading-" + index);
+    }
 
-    // Локаторы кнопок на вопросы в разделе «Вопросы о важном»
-
-    public static final By QUESTION_COST = By.id("accordion__heading-0"); // Локатор для первого вопроса (стоимость)
-    public static final By QUESTION_MULTIPLE_SCOOTERS = By.id("accordion__heading-1"); // Локатор для второго вопроса (несколько самокатов)
-    public static final By QUESTION_RENTAL_PERIOD  = By.id("accordion__heading-2"); // Локатор для третьего вопроса (время аренды)
-    public static final By QUESTION_ORDER_TODAY  = By.id("accordion__heading-3"); // Локатор для четвёртого вопроса (заказ сегодня)
-    public static final By QUESTION_ORDER_CHANGES = By.id("accordion__heading-4"); // Локатор для пятого вопроса (изменения в заказе)
-    public static final By QUESTION_SCOOTER_CHARGING  = By.id("accordion__heading-5"); // Локатор для шестого вопроса (зарядка самоката)
-    public static final By QUESTION_ORDER_CANCELLATION  = By.id("accordion__heading-6"); // Локатор для седьмого вопроса (отмена заказа)
-    public static final By QUESTION_AREA_DELIVERY  = By.id("accordion__heading-7"); // Локатор для восьмого вопроса (доставка в область)
-
+    // Ответ FAQ по индексу
+    public By faqAnswerByIndex(int index) {
+        return By.id("accordion__panel-" + index);
+    }
 
     //Тексты вопросов в разделе "Вопросы о важном"
 
@@ -43,18 +39,6 @@ public MainPage(WebDriver driver) {
     public static final String QUESTION_SCOOTER_CHARGING_TEXT  = "Вы привозите зарядку вместе с самокатом?"; // Текст вопроса о зарядке самоката
     public static final String QUESTION_ORDER_CANCELLATION_TEXT  = "Можно ли отменить заказ?"; // Текст вопроса об отмене заказа
     public static final String QUESTION_AREA_DELIVERY_TEXT  = "Я жизу за МКАДом, привезёте?"; // Текст вопроса о доставке в область
-
-
-// Локаторы для просмотра ответов в разделе «Вопросы о важном»
-
-    public static final By ANSWER_COST  = By.id("accordion__panel-0"); // Локатор для ответа на вопрос о стоимости
-    public static final By ANSWER_MULTIPLE_SCOOTERS  = By.id("accordion__panel-1"); // Локатор для ответа на вопрос о нескольких самокатах
-    public static final By ANSWER_RENTAL_PERIOD  = By.id("accordion__panel-2"); // Локатор для ответа на вопрос о времени аренды
-    public static final By ANSWER_ORDER_TODAY  = By.id("accordion__panel-3"); // Локатор для ответа на вопрос о заказе сегодня
-    public static final By ANSWER_ORDER_CHANGES  = By.id("accordion__panel-4"); // Локатор для ответа на вопрос об изменениях в заказе
-    public static final By ANSWER_SCOOTER_CHARGING  = By.id("accordion__panel-5"); // Локатор для ответа на вопрос о зарядке самоката
-    public static final By ANSWER_ORDER_CANCELLATION  = By.id("accordion__panel-6"); // Локатор для ответа на вопрос об отмене заказа
-    public static final By ANSWER_AREA_DELIVERY  = By.id("accordion__panel-7"); // Локатор для ответа на вопрос о доставке в область
 
 
 // Тексты ответов в разделе «Вопросы о важном»
@@ -70,27 +54,27 @@ public MainPage(WebDriver driver) {
 
 
 
-        //Прокрутка до вопросов
-        public void scrollToQuestions() {
-            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        }
+    //Прокрутка до вопросов
+    public void scrollToQuestions() {
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
 
-        //Проверка текста вопроса
-        public String getQuestionText(By questionLocator) {
+    // Получение текста вопроса по индексу
+    public String getQuestionTextByIndex(int index) {
+        By questionLocator = faqQuestionByIndex(index);
+        return  driver.findElement(questionLocator).getText();
+    }
 
-            return driver.findElement(questionLocator).getText();
-        }
+    // Клик по вопросу по индексу
+    public void clickQuestionByIndex(int index) {
+        By questionLocator = faqQuestionByIndex(index);
+        driver.findElement(questionLocator).click();
+    }
 
-        //Нажатие на вопрос
-        public void clickQuestion(By questionLocator) {
-
-            driver.findElement(questionLocator).click();
-        }
-
-        //Проверка текста ответа
-        public String getAnswerText(By answerLocator) {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(answerLocator));
-            return driver.findElement(answerLocator).getText();
-        }
+    // Получение текста ответа по индексу
+    public String getAnswerTextByIndex(int index) {
+        By answerLocator = faqAnswerByIndex(index);
+        return driver.findElement(answerLocator).getText();
+    }
 
     }
--- src/test/java/QuestionsPageTests.java
+++ src/test/java/QuestionsPageTests.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.openqa.selenium.By;
 import pages.MainPage;
 import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
@@ -9,31 +8,29 @@ import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class QuestionsPageTests extends BaseUITest {
-    private By questionLocator;
+    private int questionIndex;
     private String expectedQuestionText;
-    private By answerLocator;
+    private int answerIndex;
     private String expectedAnswerText;
 
-
-    public QuestionsPageTests(By question,String questionText, By answer, String answerText) {
-        this.questionLocator = question;
+    public QuestionsPageTests(int questionIndex, String questionText,int answerIndex, String answerText) {
+        this.questionIndex = questionIndex;
         this.expectedQuestionText = questionText;
-        this.answerLocator = answer;
+        this.answerIndex = answerIndex;
         this.expectedAnswerText = answerText;
     }
 
-
-    @Parameterized.Parameters(name = "Тестовые данные: кнопка вопроса={0}, ожидаемый текст вопроса={1}, реальный текст ответа={2}, ожидаемый текст ответа={3}")
+    @Parameterized.Parameters(name = "Тестовые данные: индекс вопроса={0}, ожидаемый текст вопроса={1}, реальный текст ответа={2}, ожидаемый текст ответа={3}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {MainPage.QUESTION_COST, MainPage.QUESTION_COST_TEXT, MainPage.ANSWER_COST, MainPage.ANSWER_COST_TEXT},
-                {MainPage.QUESTION_MULTIPLE_SCOOTERS , MainPage.QUESTION_MULTIPLE_SCOOTERS_TEXT, MainPage.ANSWER_MULTIPLE_SCOOTERS, MainPage.ANSWER_MULTIPLE_SCOOTERS_TEXT},
-                {MainPage.QUESTION_RENTAL_PERIOD, MainPage.QUESTION_RENTAL_PERIOD_TEXT, MainPage.ANSWER_RENTAL_PERIOD, MainPage.ANSWER_RENTAL_PERIOD_TEXT},
-                {MainPage.QUESTION_ORDER_TODAY, MainPage.QUESTION_ORDER_TODAY_TEXT, MainPage.ANSWER_ORDER_TODAY, MainPage.ANSWER_ORDER_TODAY_TEXT},
-                {MainPage.QUESTION_ORDER_CHANGES, MainPage.QUESTION_ORDER_CHANGES_TEXT, MainPage.ANSWER_ORDER_CHANGES, MainPage.ANSWER_ORDER_CHANGES_TEXT},
-                {MainPage.QUESTION_SCOOTER_CHARGING, MainPage.QUESTION_SCOOTER_CHARGING_TEXT, MainPage.ANSWER_SCOOTER_CHARGING, MainPage.ANSWER_SCOOTER_CHARGING_TEXT},
-                {MainPage.QUESTION_ORDER_CANCELLATION, MainPage.QUESTION_ORDER_CANCELLATION_TEXT, MainPage.ANSWER_ORDER_CANCELLATION, MainPage.ANSWER_ORDER_CANCELLATION_TEXT},
-                {MainPage.QUESTION_AREA_DELIVERY, MainPage.QUESTION_AREA_DELIVERY_TEXT, MainPage.ANSWER_AREA_DELIVERY, MainPage.ANSWER_AREA_DELIVERY_TEXT}
+                {0, MainPage.QUESTION_COST_TEXT,0, MainPage.ANSWER_COST_TEXT},
+                {1, MainPage.QUESTION_MULTIPLE_SCOOTERS_TEXT,1, MainPage.ANSWER_MULTIPLE_SCOOTERS_TEXT},
+                {2, MainPage.QUESTION_RENTAL_PERIOD_TEXT, 2, MainPage.ANSWER_RENTAL_PERIOD_TEXT},
+                {3, MainPage.QUESTION_ORDER_TODAY_TEXT,3,  MainPage.ANSWER_ORDER_TODAY_TEXT},
+                {4, MainPage.QUESTION_ORDER_CHANGES_TEXT,4,  MainPage.ANSWER_ORDER_CHANGES_TEXT},
+                {5, MainPage.QUESTION_SCOOTER_CHARGING_TEXT, 5, MainPage.ANSWER_SCOOTER_CHARGING_TEXT},
+                {6, MainPage.QUESTION_ORDER_CANCELLATION_TEXT, 6, MainPage.ANSWER_ORDER_CANCELLATION_TEXT},
+                {7, MainPage.QUESTION_AREA_DELIVERY_TEXT,7,  MainPage.ANSWER_AREA_DELIVERY_TEXT}
         });
     }
 
@@ -43,14 +40,12 @@ public class QuestionsPageTests extends BaseUITest {
 
         questionsPage.scrollToQuestions();
 
-        String actualQuestionText = questionsPage.getQuestionText(questionLocator);
+        String actualQuestionText = questionsPage.getQuestionTextByIndex(questionIndex);
         assertEquals("Ошибка в тексте вопроса", expectedQuestionText, actualQuestionText);
 
-        questionsPage.clickQuestion(questionLocator);
+        questionsPage.clickQuestionByIndex(questionIndex);
 
-        String actualAnswerText = questionsPage.getAnswerText(answerLocator);
+        String actualAnswerText = questionsPage.getAnswerTextByIndex(answerIndex);
         assertEquals("Ошибка в тексте ответа", expectedAnswerText, actualAnswerText);
     }
 }
-
-
